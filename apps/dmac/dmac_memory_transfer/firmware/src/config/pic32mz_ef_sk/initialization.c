46505879c82e656d8c7fd48af6cdb73f9224bc21
Arpan Kumar <arpan.kumar@microchip.com>
2020-08-17 23:22:16 +0530

    [DMAC] Regenerated demo for dmac_dmac_memory_transfer_pic32mz_ef_sk
    MH3-39403
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/dmac/dmac_memory_transfer/firmware/src/config/pic32mz_ef_sk/initialization.c
+++ apps/dmac/dmac_memory_transfer/firmware/src/config/pic32mz_ef_sk/initialization.c
@@ -147,6 +147,26 @@
 // *****************************************************************************
 // *****************************************************************************
 
+/*******************************************************************************
+  Function:
+    void STDIO_BufferModeSet ( void )
+
+  Summary:
+    Sets the buffering mode for stdin and stdout
+
+  Remarks:
+ ********************************************************************************/
+static void STDIO_BufferModeSet(void)
+{
+
+    /* Make stdin unbuffered */
+    setbuf(stdin, NULL);
+
+    /* Make stdout unbuffered */
+    setbuf(stdout, NULL);
+}
+
+
 
 
 /*******************************************************************************
@@ -164,12 +184,15 @@ void SYS_Initialize ( void* data )
     /* Start out with interrupts disabled before configuring any modules */
     __builtin_disable_interrupts();
 
+    STDIO_BufferModeSet();
+
+
   
     CLK_Initialize();
     
     /* Configure Prefetch, Wait States and ECC */
     PRECONbits.PREFEN = 3;
-    PRECONbits.PFMWS = 2;
+    PRECONbits.PFMWS = 3;
     CFGCONbits.ECCCON = 3;
 
 
