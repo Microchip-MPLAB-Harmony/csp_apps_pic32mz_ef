6d0de94ea5851a78dfcca911f4a127eb0ceb4c89
Pranjal Das <pranjal.das@microchip.com>
2024-08-13 13:40:25 +0530

    [ADCHS_DMA] Created application ADC with DMA
    MH3-50873

---
+++ apps/adchs/adchs_dma/src/config/default/peripheral/adchs/plib_adchs.c
@@ -0,0 +1,258 @@
+/*******************************************************************************
+  ADCHS Peripheral Library Interface Source File
+
+  Company
+    Microchip Technology Inc.
+
+  File Name
+    plib_adchs.c
+
+  Summary
+    ADCHS peripheral library source.
+
+  Description
+    This file implements the ADCHS peripheral library.
+
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+#include "device.h"
+#include "plib_adchs.h"
+#include "interrupts.h"
+
+#define ADCHS_CHANNEL_32  (32U)
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: ADCHS Implementation
+// *****************************************************************************
+// *****************************************************************************
+
+/* Object to hold callback function and context */
+volatile static ADCHS_CALLBACK_OBJECT ADCHS_CallbackObj[44];
+
+
+
+void ADCHS_Initialize(void)
+{
+    ADCCON1bits.ON = 0;
+    ADC3CFG = DEVADC3;
+    ADC3TIME = 0x3010001U;
+
+
+    ADCCON1 = 0x600000U;
+    ADCCON2 = 0x0U;
+    ADCCON3 = 0x1000000U;
+
+    ADCTRGMODE = 0x0U;
+
+    ADCTRG1 = 0x6000000U; 
+    ADCTRG2 = 0x0U; 
+    ADCTRG3 = 0x0U; 
+    
+    
+    
+    
+
+    ADCTRGSNS = 0x0U;
+
+    ADCIMCON1 = 0x0U;
+    ADCIMCON2 = 0x0U; 
+    ADCIMCON3 = 0x0U; 
+    
+
+    /* Input scan */
+    ADCCSS1 = 0x0U;
+    ADCCSS2 = 0x0U; 
+
+
+
+
+/* Result interrupt enable */
+ADCGIRQEN1 = 0x8U;
+ADCGIRQEN2 = 0x0U;
+/* Interrupt Enable */
+IEC1SET = 0x40000000U;
+IEC2SET = 0x0U;
+IEC3SET = 0x0U;
+
+
+
+    /* Turn ON ADC */
+    ADCCON1bits.ON = 1;
+    while(ADCCON2bits.BGVRRDY == 0U) // Wait until the reference voltage is ready
+    {
+        /* Nothing to do */
+    }
+    while(ADCCON2bits.REFFLT != 0U) // Wait if there is a fault with the reference voltage
+    {
+        /* Nothing to do */
+    }
+
+    /* ADC 3 */
+    ADCANCONbits.ANEN3 = 1;      // Enable the clock to analog bias
+    while(ADCANCONbits.WKRDY3 == 0U) // Wait until ADC is ready
+    {
+        /* Nothing to do */
+    }
+    ADCCON3bits.DIGEN3 = 1;      // Enable ADC
+
+
+
+}
+
+
+/* Enable ADCHS channels */
+void ADCHS_ModulesEnable (ADCHS_MODULE_MASK moduleMask)
+{
+    ADCCON3 |= ((uint32_t)moduleMask << 16);
+}
+
+/* Disable ADCHS channels */
+void ADCHS_ModulesDisable (ADCHS_MODULE_MASK moduleMask)
+{
+    ADCCON3 &= ~(((uint32_t)moduleMask << 16));
+}
+
+
+void ADCHS_ChannelResultInterruptEnable (ADCHS_CHANNEL_NUM channel)
+{
+    if (channel < ADCHS_CHANNEL_32)
+    {
+        ADCGIRQEN1 |= 0x01UL << channel;
+    }
+    else
+    {
+        ADCGIRQEN2 |= 0x01UL << (channel - 32U);
+    }
+}
+
+void ADCHS_ChannelResultInterruptDisable (ADCHS_CHANNEL_NUM channel)
+{
+    if (channel < ADCHS_CHANNEL_32)
+    {
+        ADCGIRQEN1 &= ~(0x01UL << channel);
+    }
+    else
+    {
+        ADCGIRQEN2 &= ~(0x01UL << (channel - 32U));
+    }
+}
+
+void ADCHS_ChannelEarlyInterruptEnable (ADCHS_CHANNEL_NUM channel)
+{
+    if (channel < ADCHS_CHANNEL_32)
+    {
+        ADCEIEN1 |= (0x01UL << channel);
+    }
+    else
+    {
+        ADCEIEN2 |= (0x01UL << (channel - 32U));
+    }
+}
+
+void ADCHS_ChannelEarlyInterruptDisable (ADCHS_CHANNEL_NUM channel)
+{
+    if (channel < ADCHS_CHANNEL_32)
+    {
+        ADCEIEN1 &= ~(0x01UL << channel);
+    }
+    else
+    {
+        ADCEIEN2 &= ~(0x01UL << (channel - 32U));
+    }
+}
+
+void ADCHS_GlobalEdgeConversionStart(void)
+{
+    ADCCON3bits.GSWTRG = 1;
+}
+
+void ADCHS_GlobalLevelConversionStart(void)
+{
+    ADCCON3bits.GLSWTRG = 1;
+}
+
+void ADCHS_GlobalLevelConversionStop(void)
+{
+    ADCCON3bits.GLSWTRG = 0;
+}
+
+void ADCHS_ChannelConversionStart(ADCHS_CHANNEL_NUM channel)
+{
+    ADCCON3bits.ADINSEL = (uint8_t)channel;
+    ADCCON3bits.RQCNVRT = 1;
+}
+
+
+/*Check if conversion result is available */
+bool ADCHS_ChannelResultIsReady(ADCHS_CHANNEL_NUM channel)
+{
+    bool status = false;
+    if (channel < ADCHS_CHANNEL_32)
+    {
+        status = (((ADCDSTAT1 >> channel) & 0x01U) != 0U);
+    }
+    else
+    {
+        status = (((ADCDSTAT2 >> (channel - 32U)) & 0x01U) != 0U);
+    }
+    return status;
+}
+
+/* Read the conversion result */
+uint16_t ADCHS_ChannelResultGet(ADCHS_CHANNEL_NUM channel)
+{
+    return (uint16_t)(*((&ADCDATA0) + channel));
+
+}
+
+void ADCHS_CallbackRegister(ADCHS_CHANNEL_NUM channel, ADCHS_CALLBACK callback, uintptr_t context)
+{
+    ADCHS_CallbackObj[channel].callback_fn = callback;
+    ADCHS_CallbackObj[channel].context = context;
+}
+
+
+
+
+bool ADCHS_EOSStatusGet(void)
+{
+    return (bool)(ADCCON2bits.EOSRDY);
+}
+
+void __attribute__((used)) ADC_DATA3_InterruptHandler(void)
+{
+    if (ADCHS_CallbackObj[3].callback_fn != NULL)
+    {
+        uintptr_t context = ADCHS_CallbackObj[3].context;
+        ADCHS_CallbackObj[3].callback_fn(ADCHS_CH3, context);
+    }
+
+    IFS1CLR = _IFS1_ADCD3IF_MASK;
+
+}
+
