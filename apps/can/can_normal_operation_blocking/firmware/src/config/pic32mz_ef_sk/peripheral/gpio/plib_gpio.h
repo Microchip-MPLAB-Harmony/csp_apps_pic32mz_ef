f5a14653ed102162161d0315d4056d33f6a75a9b
Varun A V <varun.av@microchip.com>
2024-10-24 12:55:12 +0530

    [CAN] Regenerated demo for can__can_normal_operation_blocking__firmware__pic32mz_ef_sk.X
    MH3-100968

--- apps/can/can_normal_operation_blocking/firmware/src/config/pic32mz_ef_sk/peripheral/gpio/plib_gpio.h
+++ apps/can/can_normal_operation_blocking/firmware/src/config/pic32mz_ef_sk/peripheral/gpio/plib_gpio.h
@@ -69,6 +69,7 @@
 #define LED_OutputEnable()      (TRISHCLR = (1U<<0))
 #define LED_InputEnable()       (TRISHSET = (1U<<0))
 #define LED_Get()               ((PORTH >> 0) & 0x1U)
+#define LED_GetLatch()          ((LATH >> 0) & 0x1U)
 #define LED_PIN                  GPIO_PIN_RH0
 
 
@@ -288,7 +289,7 @@ void GPIO_PortOutputEnable(GPIO_PORT port, uint32_t mask);
 
 static inline void GPIO_PinWrite(GPIO_PIN pin, bool value)
 {
-	 uint32_t xvalue = (uint32_t)value;
+     uint32_t xvalue = (uint32_t)value;
     GPIO_PortWrite((pin>>4U), (uint32_t)(0x1U) << (pin & 0xFU), (xvalue) << (pin & 0xFU));
 }
 
