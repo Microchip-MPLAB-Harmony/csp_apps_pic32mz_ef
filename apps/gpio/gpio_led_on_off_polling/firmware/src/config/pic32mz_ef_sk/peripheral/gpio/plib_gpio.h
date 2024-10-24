3ab9d07ef528f092bd6478897d9ae2bbfb87adf1
Varun A V <varun.av@microchip.com>
2024-10-24 12:53:11 +0530

    [GPIO] Regenerated demo for gpio__gpio_led_on_off_polling__firmware__pic32mz_ef_sk.X
    MH3-100992

--- apps/gpio/gpio_led_on_off_polling/firmware/src/config/pic32mz_ef_sk/peripheral/gpio/plib_gpio.h
+++ apps/gpio/gpio_led_on_off_polling/firmware/src/config/pic32mz_ef_sk/peripheral/gpio/plib_gpio.h
@@ -69,6 +69,7 @@
 #define LED_OutputEnable()      (TRISHCLR = (1U<<0))
 #define LED_InputEnable()       (TRISHSET = (1U<<0))
 #define LED_Get()               ((PORTH >> 0) & 0x1U)
+#define LED_GetLatch()          ((LATH >> 0) & 0x1U)
 #define LED_PIN                  GPIO_PIN_RH0
 
 /*** Macros for SWITCH pin ***/
@@ -78,6 +79,7 @@
 #define SWITCH_OutputEnable()      (TRISBCLR = (1U<<12))
 #define SWITCH_InputEnable()       (TRISBSET = (1U<<12))
 #define SWITCH_Get()               ((PORTB >> 12) & 0x1U)
+#define SWITCH_GetLatch()          ((LATB >> 12) & 0x1U)
 #define SWITCH_PIN                  GPIO_PIN_RB12
 
 
@@ -297,7 +299,7 @@ void GPIO_PortOutputEnable(GPIO_PORT port, uint32_t mask);
 
 static inline void GPIO_PinWrite(GPIO_PIN pin, bool value)
 {
-	 uint32_t xvalue = (uint32_t)value;
+     uint32_t xvalue = (uint32_t)value;
     GPIO_PortWrite((pin>>4U), (uint32_t)(0x1U) << (pin & 0xFU), (xvalue) << (pin & 0xFU));
 }
 
